96a90afc9a740530180d7209e5c40e6be690033d
VoxLight <VoxLight@users.noreply.github.com>
2023-08-08 14:01:31 +0000

    Updated new logger and default event messages.

--- nextcordbot/nextcordbot.py
+++ nextcordbot/nextcordbot.py
@@ -17,21 +17,32 @@ class NextcordBot(commands.Bot):
 
         # Help with the logger: 
         # https://docs.nextcord.dev/en/stable/logging.html
-        self.logger = logging.getLogger('nextcord')
-        self.logger.setLevel(logging.DEBUG)
+        # Set up logging handler for nextcord logs
+        nextcord_logs = logging.getLogger('nextcord')
+        nextcord_logs.setLevel(logging.DEBUG)
         _handler = logging.FileHandler(
-            filename=os.path.join(up_dir(__file__), 'logs', f'nextcord{datetime.datetime.now().strftime("%h-%s")}.log'),
+            filename=os.path.join(up_dir(__file__), 'logs', 'nextcord.log'),
+            encoding='utf-8',
+            mode='w'
+        )
+        nextcord_logs.addHandler(_handler)
+
+        # Set up the internal logger
+        self.log = logging.getLogger('bot')
+        self.log.setLevel(logging.DEBUG)
+        _handler = logging.FileHandler(
+            filename=os.path.join(up_dir(__file__), 'logs', f'bot {datetime.datetime.now().strftime("%h-%s")}.log'),
             encoding='utf-8',
             mode='w'
         )
         _handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-        self.logger.addHandler(_handler)
+        self.log.addHandler(_handler)
 
     def __call__(self):
         self.run(os.getenv("DISCORD_BOT_TOKEN"))
 
     async def on_ready(self):
-        print("On Ready")
+        self.log.info("On Ready!")
 
-    async def on_message(self):
-        print("On Message")
+    async def on_message(self, message: nextcord.Message):
+        print("Message Sent:", message.created_at, message.content)
